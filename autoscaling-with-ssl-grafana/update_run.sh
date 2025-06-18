99b229d4aa3e041acd57b3560c41dcd0c4e551f5
iolesyk <iolesyk@gmail.com>
2025-06-18 15:06:27 -0400

    DEV-955 - add support latest SM2.0 release build 14.2.1

--- autoscaling-with-ssl-grafana/update_run.sh
+++ autoscaling-with-ssl-grafana/update_run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # This script is required to run kafka cluster (without zookeeper)
+#
+# export KAFKA_CLUSTER_ID is required to run kafka
+
+if [ -z "$KAFKA_CLUSTER_ID" ]; then
+    echo "KAFKA_CLUSTER_ID is required to run kafka"
+    exit 1
+fi
 
 # Docker workaround: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
 sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
@@ -8,7 +15,7 @@ sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
 # sed -i 's/cub zk-ready/echo ignore zk-ready/' /etc/confluent/docker/ensure
 
 # KRaft required step: Format the storage directory with a new cluster ID
-echo "export CLUSTER_ID=$(kafka-storage random-uuid)" >> /etc/confluent/docker/bash-config
+echo "export CLUSTER_ID=$KAFKA_CLUSTER_ID" >>/etc/confluent/docker/bash-config
 
 # Update max message size
 # (Needed by TerraformService to store terraform-state files)
